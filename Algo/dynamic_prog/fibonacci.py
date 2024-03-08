199611f6b8ed72201f90cf4365ad4480149e79b8
Kumar Sahastranshu <krsahas.git@gmail.com>
2024-03-08 22:19:50 +0530

    fibonacci series with memoization

--- Algo/dynamic_prog/fibonacci.py
+++ Algo/dynamic_prog/fibonacci.py
@@ -1,12 +1,16 @@
+import time
+
 # Finonacci series: 1,1,2,3,5,8,13,21,34,...
 # Time: 2^n
 
-def fib(n):
-    if n <= 2: return 1
-    return fib(n-1)+fib(n-2)
+# Fibonacci series with memoization to improve performance
+fib_series = [1, 1]
+def fib(n, memo={1: 1, 2: 1}):
+    if n in memo.keys(): return (memo[n], fib_series)
+    if n <= 2: return (1, fib_series)
+    memo[n] = fib(n-1, memo)[0]+fib(n-2, memo)[0]
+    fib_series.append(memo[n])
+    return (memo[n], fib_series)
 
-try: 
-    num = int(input())
-    # this is pretty slow for larger value of num
-    print (fib(num))
-except: print ("Only positive number is allowed.")
+print ('for 10: ', fib(10))
+
